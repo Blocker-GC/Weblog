aaae2037367ab610056ee9646fb795422bd04d8c
MaxLangen <max.langen@student.graafschapcollege.nl>
2016-09-19 09:29:49 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,8 @@
+<div class="container">
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Welkom</h1>
+        <p>Op het weblog systeem. Klik rechts op inloggen om in te loggen.</p>
+        <p><img src="/img/homeintro.jpg" class="img-responsive" alt="Home intro"></p>
+      </div>
+</div>
