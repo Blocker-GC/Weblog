643bf58a1d7d094469d66bf8c6c693631754f2bf
MaxLangen <max.langen@student.graafschapcollege.nl>
2016-09-19 09:28:29 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>Project</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/navbar.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.8.3.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+  
+ </head>
+ <body>
+
+
+ <div class="container">
+<div class="logo">
+<img src="/img/logo.png" alt="Logo" class="img-responsive">
+</div>
+    <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <!--<div class="container-fluid"> -->
+          
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Uitleg</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Inloggen</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+         <!--</div><!--/.container-fluid -->
+      </nav>
+</div>
+
+    
